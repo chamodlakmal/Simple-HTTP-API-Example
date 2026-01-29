50805ee2a52027256712fef2cf5c867ed2e642d0
Chamod <chamod.lakmal@pickme.lk>
2026-01-29 10:00:12 +0530

    initial commit

---
+++ src/main/java/lk/chamiviews/simple_http_api/SimpleHttpApiApplication.java
@@ -0,0 +1,13 @@
+package lk.chamiviews.simple_http_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHttpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleHttpApiApplication.class, args);
+	}
+
+}
